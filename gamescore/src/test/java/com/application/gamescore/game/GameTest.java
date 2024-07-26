9fdbb73a8af1038c95cb413f60d35fa36fc28d75
junhyunpark <junhyunpark305@gmail.com>
2024-07-26 19:45:27 +0900

    testcode 주석

--- gamescore/src/test/java/com/application/gamescore/game/GameTest.java
+++ gamescore/src/test/java/com/application/gamescore/game/GameTest.java
@@ -1,22 +1,17 @@
-package com.application.gamescore.game;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.application.gamescore.game.dao.GameDAO;
-
-@SpringBootTest
-public class GameTest {
-	
-	@Autowired
-	public GameDAO gameDAO;
-	
-	@DisplayName("dd")
-	@Test
-	public void dd() {
-		
-	}
-	
-}
+//package com.application.gamescore.game;
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.application.gamescore.game.dao.GameDAO;
+//
+//@SpringBootTest
+//public class GameTest {
+//	
+//	@Autowired
+//	public GameDAO gameDAO;
+//	
+//	
+//}
--- gamescore/src/test/java/com/application/gamescore/user/UserTest.java
+++ gamescore/src/test/java/com/application/gamescore/user/UserTest.java
@@ -1,68 +1,68 @@
-package com.application.gamescore.user;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.application.gamescore.user.dao.UserDAO;
-import com.application.gamescore.user.dto.UserDTO;
-
-
-@SpringBootTest
-public class UserTest {
-	
-	@Autowired
-	public PasswordEncoder passwordEncoder;
-	
-	@Autowired
-	public UserDAO userDAO;
-	
-	@DisplayName("register")
-	@Test
-	public void register() {
-		
-		UserDTO userDTO = new UserDTO();
-		userDTO.setUserId(100);
-		userDTO.setUserNm("test");
-		userDTO.setUserPw("sssss");
-		userDTO.setAddress("test");
-		userDTO.setZipcode("000000");
-		userDTO.setRoadAddress("111-111");
-		userDTO.setEmail("test@test.com");
-		
-		userDAO.register(userDTO);
-		
-	}
-	
-	
-	@DisplayName("checkDuplicatedId")
-	@Test
-	public void checkDuplicatedId() {
-		
-		String userNm = "testNm";
-		
-		String result = userDAO.checkDuplicatedId(userNm);
-		System.out.println(result);
-		
-	}
-	
-	@DisplayName("login")
-	@Test
-	public void login() {
-		
-		UserDTO userDTO = new UserDTO();
-		userDTO.setUserNm("brain305");
-		userDTO.setUserPw("wnsgus305");
-		
-		UserDTO checkExsistId = userDAO.login(userDTO);
-		if (checkExsistId != null) {
-			if (passwordEncoder.matches(userDTO.getUserPw() ,checkExsistId.getUserPw())) {
-				
-				System.out.println("성공");
-				
-			}
-		}
-	}
-}
+//package com.application.gamescore.user;
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import com.application.gamescore.user.dao.UserDAO;
+//import com.application.gamescore.user.dto.UserDTO;
+//
+//
+//@SpringBootTest
+//public class UserTest {
+//	
+//	@Autowired
+//	public PasswordEncoder passwordEncoder;
+//	
+//	@Autowired
+//	public UserDAO userDAO;
+//	
+//	@DisplayName("register")
+//	@Test
+//	public void register() {
+//		
+//		UserDTO userDTO = new UserDTO();
+//		userDTO.setUserId(100);
+//		userDTO.setUserNm("test");
+//		userDTO.setUserPw("sssss");
+//		userDTO.setAddress("test");
+//		userDTO.setZipcode("000000");
+//		userDTO.setRoadAddress("111-111");
+//		userDTO.setEmail("test@test.com");
+//		
+//		userDAO.register(userDTO);
+//		
+//	}
+//	
+//	
+//	@DisplayName("checkDuplicatedId")
+//	@Test
+//	public void checkDuplicatedId() {
+//		
+//		String userNm = "testNm";
+//		
+//		String result = userDAO.checkDuplicatedId(userNm);
+//		System.out.println(result);
+//		
+//	}
+//	
+//	@DisplayName("login")
+//	@Test
+//	public void login() {
+//		
+//		UserDTO userDTO = new UserDTO();
+//		userDTO.setUserNm("brain305");
+//		userDTO.setUserPw("wnsgus305");
+//		
+//		UserDTO checkExsistId = userDAO.login(userDTO);
+//		if (checkExsistId != null) {
+//			if (passwordEncoder.matches(userDTO.getUserPw() ,checkExsistId.getUserPw())) {
+//				
+//				System.out.println("성공");
+//				
+//			}
+//		}
+//	}
+//}
